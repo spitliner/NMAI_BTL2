3eaaddbc64d293c3793c1d51fc7f849ae741cb10
spitliner <>
2022-05-15 17:47:19 +0700

    add code for mem monitoring

---
+++ testCase.py
@@ -0,0 +1,15 @@
+import importlib
+import sys
+
+
+othello_ai = importlib.import_module("othello-match")
+
+def main(argv):
+    print(int(argv[0]))
+    othello_ai.playGame(int(argv[0]), 0)
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        main(sys.argv[1:])
+    else:
+        main(["1"])
